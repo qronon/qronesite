578f9b5ed3568310a1b7b3c0f6955fcf86811799
J.Tabuchi <qrononx@yahoo.co.jp>
2012-01-07 01:24:17 +0900

    Update htdocs/reference.server.js

--- htdocs/reference.server.js
+++ htdocs/reference.server.js
@@ -4,4 +4,4 @@ header("Content-Type: text/html; charset=utf8");
 
 document.load("htdocs/reference.html");
 document.set(load_yaml("reference" + query.path + ".yaml"));
-document.out();
+document.out(); 
